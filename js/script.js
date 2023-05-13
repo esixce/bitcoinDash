670b2c9f47de5212fceda9323137e6846923c05c
esixce <98708432+esixce@users.noreply.github.com>
2023-05-13 16:10:30 -0500

    framework

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@
         menuContent.style.width = sidebarContent.classList.contains("active") ? "calc(100% - 200px)" : "100%";
         menuContent.style.marginLeft = sidebarContent.classList.contains( "active" ) ? "200px" : "0";
 
-        mainContent.style.top = "10px";
+        mainContent.style.top = "0";
         mainContent.style.width = sidebarContent.classList.contains("active") ? "calc(100% - 200px)" : "100%";
         mainContent.style.marginLeft = sidebarContent.classList.contains( "active" ) ? "200px" : "0";
 
