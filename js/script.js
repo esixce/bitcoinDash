77f434258ed5320a83ffc676d4e6516065a29089
esixce <98708432+esixce@users.noreply.github.com>
2023-05-14 11:10:09 -0500

    framework

--- js/script.js
+++ js/script.js
@@ -1740,7 +1740,7 @@ $(function () {
     );
   }
 
-  function lineGraph(ddata, tag) {
+  function lineGraphM(ddata, tag) {
     // set the dimensions and margins of the graph
     const margin = { top: 10, right: 30, bottom: 30, left: 60 },
       width = 460 - margin.left - margin.right,
