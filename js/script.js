f442e116415f29a5179fce63690d010d70dcc167
esixce <98708432+esixce@users.noreply.github.com>
2023-05-14 10:55:04 -0500

    framework

--- js/script.js
+++ js/script.js
@@ -99,10 +99,7 @@ $(function () {
     actHTML();
   };
   //FUNCTION
-  function generateSocialMediaPost() {
-
-
-  }
+  function generateSocialMediaPost() {}
 
   // HOME COMPONENT
   function mainHtml() {
@@ -162,21 +159,6 @@ $(function () {
             );
             var overviewHtml = insertProperty(
               overviewHtml,
-              "pruneheight",
-              data.pruneheight
-            );
-            var overviewHtml = insertProperty(
-              overviewHtml,
-              "automatic_pruning",
-              data.automatic_pruning
-            );
-            var overviewHtml = insertProperty(
-              overviewHtml,
-              "softforks",
-              data.softforks
-            );
-            var overviewHtml = insertProperty(
-              overviewHtml,
               "warnings",
               data.warnings
             );
@@ -199,6 +181,9 @@ $(function () {
         cardHTML(homeCards.cardA);
         cardHTML(homeCards.cardB);
         cardHTML(homeCards.cardC);
+        cardHTML(homeCards.cardD);
+        cardHTML(homeCards.cardE);
+        cardHTML(homeCards.cardF);
       },
       false
     );
@@ -255,18 +240,29 @@ $(function () {
         $ajaxUtils.sendGetRequest(
           urls.baseUrl + card.url,
           function (data) {
-            treemapSmallD(data, "#" + card.chart);
+            lineGraph(data, "#" + card.chart);
           },
           true
         );
         break;
       case "E":
-        // ... and so on
+        $ajaxUtils.sendGetRequest(
+          urls.baseUrl + card.url,
+          function (data) {
+            lineGraph(data, "#" + card.chart);
+          },
+          true
+        );
         break;
       case "F":
-        // ... and so on
+        $ajaxUtils.sendGetRequest(
+          urls.baseUrl + card.url,
+          function (data) {
+            lineGraph(data, "#" + card.chart);
+          },
+          true
+        );
         break;
-      // ... and so on
     }
   }
 
@@ -1669,6 +1665,80 @@ $(function () {
   }
 
   // TEST ZONE
+
+  function lineGraph(ddata, tag) {
+    // set the dimensions and margins of the graph
+    const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+      width = 460 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3
+      .select(tag)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    //Read the data
+    d3.csv(
+      "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+
+      // When reading the csv, I must format variables:
+      function (d) {
+        return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value };
+      }
+    ).then(
+      // Now I can use this dataset:
+      function (data) {
+        // Add X axis --> it is a date format
+        const x = d3
+          .scaleTime()
+          .domain(
+            d3.extent(data, function (d) {
+              return d.date;
+            })
+          )
+          .range([0, width]);
+        svg
+          .append("g")
+          .attr("transform", `translate(0, ${height})`)
+          .call(d3.axisBottom(x));
+
+        // Add Y axis
+        const y = d3
+          .scaleLinear()
+          .domain([
+            0,
+            d3.max(data, function (d) {
+              return +d.value;
+            }),
+          ])
+          .range([height, 0]);
+        svg.append("g").call(d3.axisLeft(y));
+
+        // Add the line
+        svg
+          .append("path")
+          .datum(data)
+          .attr("fill", "none")
+          .attr("stroke", "steelblue")
+          .attr("stroke-width", 1.5)
+          .attr(
+            "d",
+            d3
+              .line()
+              .x(function (d) {
+                return x(d.date);
+              })
+              .y(function (d) {
+                return y(d.value);
+              })
+          );
+      }
+    );
+  }
 
   function bigBarChart(data, tag) {
     // console.log(data);
